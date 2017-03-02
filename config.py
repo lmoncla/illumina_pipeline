e85c63aaaccbb4965fd11f49cf8d322d533b4945
Louise H Moncla <lhmoncla@gmail.com>
2017-03-02 10:56:20 -0600

    initial commit

---
+++ config.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+
+class configuration(object):
+	def __init__(self):
+				
+
+####### SPECIFY WHICH TASKS YOU WANT TO DO HERE #########################################
+		
+		# for each of the below (trimming, mapping and calling SNPs), set to true if you want to do it, set to false if you do not. If you set something to false then you do not need to change any of the parameters for the associated analysis
+		self.trim = False
+		self.map = False
+		self.call_snps = True
+		self.de_novo_assembly = False
+		self.de_novo_assemble_mapped_reads = False
+		self.calculate_genewise_pi = False
+		self.calculate_genewise_piNpiS = False
+		self.calculate_sliding_window_piNpiS = False
+		
+		
+		
+####### SET/ALTER PARAMETERS ############################################################
+		
+####### SET TRIMMING PARAMETERS #########################################################
+### Fill these out if self.trim = True		
+		
+		# after trimming, discard reads below this length
+		self.minlength = 100
+		
+		# during trimming, slide along reads in windows of this size (in base pairs)
+		self.window_size = 5
+		
+		# trim reads using a quality score threshold of this (for Q30, set to 30, etc...)
+		self.trim_qscore = 30
+		
+		
+###### SPECIFY REFERENCE SEQUENCE ########################################################
+### Fill these out if self.map = True		
+		
+		# here, put the full path for the reference sequence you wish to use for mapping. You can figure out the full path by dragging and dropping the file into the terminal, and then copying that file path into here
+		self.reference_sequence = "/Volumes/LaCie/Users/lhmoncla/Documents/New_Illumina_Pipeline_Development/pipeline_testing/reference_based_mapping_pipeline/CA04_HA_GQ117044.fa"
+		
+		# specify what you would like your reference sequence to be named
+		self.reference_sequence_name = "CA04_HA_GQ117044.fa"
+		
+		# If, instead of mapping everything to the same reference you would like to map sample to a different reference, then specify True here. This will also require that the references you wish to use have been placed in the same folder as the trimmed fastq files. 
+		self.use_different_reference_for_each_sample = True		
+
+####### SET SNP CALLING PARAMETERS ######################################################
+### Fill these out if self.call_snps = True		
+		
+		# pick whether you would like to use LoFreq or Varscan to call variants
+		self.use_lofreq = True
+		self.use_varscan = True
+				
+		# minimum coverage, i.e., SNPs will not be called at sites that have coverage less than this value
+		self.min_cov = 100
+		
+		# set base quality threshold, i.e., SNPs will not be called for bases that have a Qscore below this value
+		self.snp_qual_threshold = 30
+		
+		# set SNP frequency cutoff (1% would be specified as 0.01). SNPs present below this frequency will not be reported
+		self.snp_frequency = 0.01		
+		
+
+####### SPECIFY POPOOLATION PARAMETERS ##################################################
+### Fill these out if self.calculate_genewise_pi, self.calculate_genewise_piNpiS, or self.calculate_sliding_window_pi = True		
+		
+		# Specify whether each sample should have its own gtf (set to True) or if the same one should be used for all (set to False)
+		self.use_different_gtf_for_each_sample = True
+		
+		# if using a shared gtf for all files, specify the path to that gtf here; if using a different gtf for each sample, this isn't necessary, and the program will just assume the gtf is in the folder with the fastq files
+		self.gtf_location = "/Directory/of/gtf"
+
+		# set to True if you would like to subsample the pileup file. Then set desired coverage level with subsample_level = desired depth
+		self.subsample = False
+		self.subsample_level = 1000
+		
+		# this sets the --min-count parameter in popoolation, which is the minimum count of the minor allele for it to be counted
+		self.min_count = 1
+		
+		# this sets the --min-coverage parameter in popoolation; sites with coverage less than this value will not be used for calculating pi
+		self.min_coverage = 100 
+		
+		# this sets the --max-coverage parameter in popoolation; sites with coverage exceeding this value will not be used for calculating pi
+		self.max_coverage = 1000000
+		
+		# this sets the --nonsyn-length-table in popoolation, which specifies which codon table you want to use (you can choose nsl-P1.txt or nsl-P2.txt)
+		self.nonsyn_length_table = "nsl_p1.txt"
+		
+		
+
+### Fill these out if self.calculate_sliding_window_pi = True		
+
+		# for sliding window pi analyses, this specifies the --window-size and --step-size parameters; popoolation will slide along the gene in windows of size pi_step_size, calculating pi in windows of size pi_window_size
+		self.pi_window_size = 9
+		self.pi_step_size = 3
+		
+
+
+
+
+### NOTES ###
+# To run the pipeline, navigate to the directory that contains your fastq files and run illumina_pipeline.py and the configuration file you wish to use. 
+# This configuration file will be used to inform the commands run by illumina_pipeline.py. The pipeline uses Trimmomatic for trimming, bowtie2 for mapping and lofreq for variant calling. I will assume that if you are using this pipeline that you have read the documentation for these programs and understand how they work and what impact these parameters have on the analysis. For specific information about these programs, please consult their online documentation atTrimmomatic: http://www.usadellab.org/cms/?page=trimmomatic, and bowtie2: http://bowtie-bio.sourceforge.net/bowtie2/index.shtml. 
---
+++ illumina_pipeline_fastq_to_snps.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+
+import sys, subprocess, glob, os, shutil, re, importlib
+from subprocess import call
+
+config_filename = sys.argv[1]
+config = __import__(config_filename)						# imports config_filename as a module
+cfg = config.configuration()								# make cfg an instance of class configuration accessed from the config module
+
+file_list = []
+sample_list = []
+sample_dict = {}
+reference_list = []
+
+
+# make a list of all the fastq files in the directory and store their sample name as the fastq file name without the R1_001 or R2_001. 
+for file in glob.glob("*.fastq"):						# glob will find instances of all files ending in .fastq as shell would
+	file_list.append(file)
+	
+for file in file_list:									# find all reads that are pairs
+	if "R1" in file:
+		samplename = file.replace("_R1_001", "")		# rename R1 reads to take out the R1_001.fastq
+		samplename = samplename.replace(".fastq", "")
+	elif "R2" in file:
+		samplename = file.replace("_R2_001", "")		# rename R2 reads to take out the R2_001.fastq
+		samplename = samplename.replace(".fastq", "")	
+	else:
+		samplename=file									# if there are only nonpaired reads, just keep the same name
+			
+	sample_list.append(samplename)
+	
+	# set up sample_dictionary with the sample name as keys and filenames as dictionaries
+	if samplename in sample_dict:
+		sample_dict[samplename].append(file)
+		
+	else: 
+		sample_dict[samplename] = [file]
+
+sample_list = list(set(sample_list))					# keep only unique list entries
+
+
+# combine R1 and R2 fastq files and move into directories based on their sample name; if using different references for each mapping, this will also move each reference sequence into the sample folder
+def combine_fastqfiles():
+
+	# make folders for each sample name and combine the forward and reverse reads into each folder
+	for s in sample_dict:
+		call("mkdir {s}".format(s=s), shell=True)
+	
+		for file in file_list:
+			if s in file:
+				call("cp {file} {s}/{file}".format(s=s, file=file), shell=True)
+				
+
+# perform trimming
+def trim(sample_list):
+	for s in sample_dict:
+		for value in sample_dict[s]:
+			call("java -jar /usr/local/bin/Trimmomatic-0.36/trimmomatic-0.36.jar SE {s}/{value} {s}/{value}.trimmed.fastq SLIDINGWINDOW:{window}:{qscore} MINLEN:{MINLEN}".format(s=s, value=value, MINLEN=cfg.minlength, window=cfg.window_size,qscore=cfg.trim_qscore), shell=True)
+	
+	
+
+# perform mapping
+def map(sample_list):
+	
+	if cfg.use_different_reference_for_each_sample == False:
+		call("bowtie2-build {reference_sequence} {reference_sequence_name}".format(reference_sequence=cfg.reference_sequence, reference_sequence_name=cfg.reference_sequence_name), shell=True)
+		for s in sample_dict:
+			call("bowtie2 -x {reference_sequence} -U {s}/{f1}.trimmed.fastq,{s}/{f2}.trimmed.fastq -S {s}/{s}.sam --local".format(s=s, reference_sequence=cfg.reference_sequence, f1=sample_dict[s][0], f2=sample_dict[s][1]), shell=True)
+	
+	
+	elif cfg.use_different_reference_for_each_sample == True:		
+		for s in sample_dict:
+			for file in os.listdir(s):
+				if file.endswith(".fasta") or file.endswith(".fa"):
+					reference_name = file
+			call("rm {s}/bowtie_reference_files".format(s=s), shell=True)
+			call("bowtie2-build {s}/{reference_name} {s}/{reference_name}".format(s=s, reference_name=reference_name), shell=True)
+			call("mkdir {s}/bowtie_reference_files; cd {s}/; for f in *.bt2; do mv $f bowtie_reference_files/$f; done".format(s=s), shell=True)
+			call("bowtie2 -x {s}/bowtie_reference_files/{reference_name} -U {s}/{f1}.trimmed.fastq,{s}/{f2}.trimmed.fastq -S {s}/{s}.sam --local".format(s=s, reference_name=reference_name, f1=sample_dict[s][0], f2=sample_dict[s][1]), shell=True)
+		
+
+def call_SNPs():
+	for s in sample_dict:
+		#convert sam to bam, then sort bam file
+		call("samtools view -bS {s}/{s}.sam > {s}/{s}.bam".format(s=s), shell=True)
+		call("samtools sort {s}/{s}.bam > {s}/{s}.sorted.bam".format(s=s), shell=True)
+		call("rm {s}/{s}.lofreq.{freq}.vcf; rm {s}/{s}.filtered.{freq}.vcf".format(s=s,freq=cfg.snp_frequency), shell=True)
+		
+		# assign names to snpEff_ref_name variable, which will be used for amino acid annotation with snpEff
+		if ".fasta" in cfg.reference_sequence_name: 
+			snpEff_ref_name = cfg.reference_sequence_name.replace(".fasta", "")
+		if ".fa" in cfg.reference_sequence_name: 
+			snpEff_ref_name = cfg.reference_sequence_name.replace(".fa", "")
+
+	
+		# run SNP calling and amino acid change annotations if samples are all mapped to the same reference
+		if cfg.use_different_reference_for_each_sample == False:
+			# call variants with lofreq and filter them
+			if cfg.use_lofreq == True:
+				print "now calling SNPs on %s using lofreq" % s
+				call("lofreq call -f {reference_sequence} -o {s}/{s}.lofreq.{snp_frequency}.vcf {s}/{s}.sorted.bam".format(s=s, snp_frequency=cfg.snp_frequency, reference_sequence=cfg.reference_sequence), shell=True)	
+				call("lofreq filter --cov-min {min_cov} --snvqual-thresh {snp_qual_threshold} --af-min {snp_frequency} -i {s}/{s}.lofreq.{snp_frequency}.vcf -o {s}/{s}.filtered.{snp_frequency}.vcf".format(s=s, min_cov=cfg.min_cov, snp_qual_threshold=cfg.snp_qual_threshold, snp_frequency=cfg.snp_frequency), shell=True)
+				call("java -jar /usr/local/bin/snpEff/snpEff.jar {snpEff_ref_name} {s}/{s}.lofreq.filtered.{snp_frequency}.vcf > {s}/{s}.lofreq.annotated.{snp_frequency}.vcf".format(snpEff_ref_name=snpEff_ref_name,s=s,snp_frequency=cfg.snp_frequency), shell=True)
+
+			if cfg.use_varscan == True:
+				print "now calling SNPs on %s using varscan" % s
+				call("samtools mpileup -d1000000 {s}/{s}.sorted.bam > {s}/{s}.pileup -f {reference_sequence}".format(s=s, reference_sequence=cfg.reference_sequence), shell=True)
+				call("java -jar /usr/local/bin/VarScan.v2.3.9.jar mpileup2snp {s}/{s}.pileup --min-coverage {min_cov} --min-avg-qual {snp_qual_threshold} --min-var-freq {snp_frequency} --strand-filter 1 --output-vcf 1 > {s}/{s}.varscan.snps.{snp_frequency}.vcf".format(s=s,snp_frequency=cfg.snp_frequency,min_cov=cfg.min_cov, snp_qual_threshold=cfg.snp_qual_threshold), shell=True)		
+				call("java -jar /usr/local/bin/snpEff/snpEff.jar {snpEff_ref_name} {s}/{s}.varscan.snps.{snp_frequency}.vcf > {s}/{s}.varscan.snps.annotated.{snp_frequency}.vcf".format(snpEff_ref_name=snpEff_ref_name,s=s,snp_frequency=cfg.snp_frequency), shell=True)
+	
+		
+		
+		# run SNP calling and amino acid change annotations if samples are mapped to their own references
+		elif cfg.use_different_reference_for_each_sample == True:		
+			for file in os.listdir(s):
+				if file.endswith(".fasta") or file.endswith(".fa"):
+					reference_sequence = file
+			
+			# assign snpEff reference names
+			if ".fasta" in reference_sequence: 
+				snpEff_ref_name = reference_sequence.replace(".fasta", "")
+			elif ".fa" in reference_sequence: 
+				snpEff_ref_name = reference_sequence.replace(".fa", "")				
+			
+			if cfg.use_lofreq == True: 
+				print "now calling SNPs on %s using lofreq" % s
+				call("lofreq call -f {s}/{reference_sequence} -o {s}/{s}.lofreq.{snp_frequency}.vcf {s}/{s}.sorted.bam".format(s=s, snp_frequency=cfg.snp_frequency, reference_sequence=reference_sequence), shell=True)	
+				call("lofreq filter --cov-min {min_cov} --snvqual-thresh {snp_qual_threshold} --af-min {snp_frequency} -i {s}/{s}.lofreq.{snp_frequency}.vcf -o {s}/{s}.lofreq.filtered.{snp_frequency}.vcf".format(s=s, min_cov=cfg.min_cov, snp_qual_threshold=cfg.snp_qual_threshold, snp_frequency=cfg.snp_frequency), shell=True)
+				call("java -jar /usr/local/bin/snpEff/snpEff.jar {snpEff_ref_name} {s}/{s}.lofreq.filtered.{snp_frequency}.vcf > {s}/{s}.lofreq.annotated.{snp_frequency}.vcf".format(snpEff_ref_name=snpEff_ref_name,s=s,snp_frequency=cfg.snp_frequency), shell=True)
+	
+			if cfg.use_varscan == True: 
+				print "now calling SNPs on %s using varscan" % s
+				call("samtools mpileup -d 1000000 {s}/{s}.sorted.bam > {s}/{s}.pileup -f {s}/{reference_sequence}".format(s=s, reference_sequence=reference_sequence), shell=True)
+				call("java -jar /usr/local/bin/VarScan.v2.3.9.jar mpileup2snp {s}/{s}.pileup --min-coverage {min_cov} --min-avg-qual {snp_qual_threshold} --min-var-freq {snp_frequency} --strand-filter 1 --output-vcf 1 > {s}/{s}.varscan.snps.{snp_frequency}.vcf".format(s=s,snp_frequency=cfg.snp_frequency,min_cov=cfg.min_cov, snp_qual_threshold=cfg.snp_qual_threshold), shell=True)
+				call("java -jar /usr/local/bin/snpEff/snpEff.jar {snpEff_ref_name} {s}/{s}.varscan.snps.{snp_frequency}.vcf > {s}/{s}.varscan.snps.annotated.{snp_frequency}.vcf".format(snpEff_ref_name=snpEff_ref_name,s=s,snp_frequency=cfg.snp_frequency), shell=True)
+
+				
+		call("cd {s}; rm -rf snp_calls; mkdir snp_calls; for f in *.vcf; do mv $f snp_calls/$f; done".format(s=s), shell=True)
+		
+	if cfg.use_lofreq == True: 			
+		call("rm combined.{snp_frequency}.snps.txt; grep -r --include='*.lofreq.annotated.{snp_frequency}.vcf' . * >> combined.{snp_frequency}.snps.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'/#/d' combined.{snp_frequency}.snps.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'s/\;/\t/g' combined.{snp_frequency}.snps.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'s/\:/\t/g' combined.{snp_frequency}.snps.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'s/\t*\=/\t/g' combined.{snp_frequency}.snps.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'s/\.fastq.*\.vcf//g' combined.{snp_frequency}.snps.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		
+	
+	if cfg.use_varscan == True: 
+		call("rm combined.{snp_frequency}.snps.txt; grep -r --include='*.varscan.snps.annotated.{snp_frequency}.vcf' . * >> varscan.snps.{snp_frequency}.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'/Position/d' varscan.snps.{snp_frequency}.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'s/\:/\t/g' varscan.snps.{snp_frequency}.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+		call("sed -i '' $'s/\.fastq.*\.txt//g' varscan.snps.{snp_frequency}.txt".format(snp_frequency=cfg.snp_frequency), shell=True)
+
+
+
+# perform de novo assembly with Trinity
+def de_novo_assembly():
+	# perform de novo assembly using trinity on the trimmed fastq files
+	for s in sample_dict:
+		fastq1 = sample_dict[s][0]+".trimmed.fastq"
+		fastq2 = sample_dict[s][1]+".trimmed.fastq"
+		call("/usr/local/bin/trinityrnaseq-Trinity-v2.4.0/Trinity --seqType fq --single {s}/{fastq1},{s}/{fastq2} --max_memory 3G --output {s}/trinity_output".format(s=s,fastq1=fastq1, fastq2=fastq2), shell=True)
+
+		# pipe the output to blastn to return the identity of the contigs
+		call("/usr/local/bin/ncbi-blast-2.6.0+/bin/blastn -db nt -query {s}/trinity_output/Trinity.fasta -out {s}/Trinity_BLAST_result.txt -max_target_seqs 10 -outfmt '7 qseqid sseqid pident length evalue stitle qcovhsp qstart qend sstart send' -remote".format(s=s), shell=True)
+	
+
+def de_novo_assemble_mapped_reads():
+	# perform de novo assembly using trinity on the trimmed fastq files
+	for s in sample_dict:
+		fastq1 = sample_dict[s][0]+".trimmed.fastq"
+		fastq2 = sample_dict[s][1]+".trimmed.fastq"	
+		
+		# cp the original and trimmed fastq files into the new directory, remove the white spaces and replace with _s
+		call("mkdir {s}/trinity_de_novo_assembly_mapped_reads_only; cp {s}/{fastq1} {s}/trinity_de_novo_assembly_mapped_reads_only/{fastq1}.nospaces.fastq; cp {s}/{fastq2} {s}/trinity_de_novo_assembly_mapped_reads_only/{fastq2}.nospaces.fastq".format(s=s, fastq1=fastq1, fastq2=fastq2), shell=True)
+		call("for f in {s}/trinity_de_novo_assembly_mapped_reads_only/*.fastq; do sed -i '' $'s/\ /\_/g' $f; done".format(s=s), shell=True)
+		
+		# redo bowtie mapping and put the white spaces back into the sam file
+		for file in os.listdir(s):
+				if file.endswith(".fasta") or file.endswith(".fa"):
+					reference_name = file
+		call("rm {s}/bowtie_reference_files".format(s=s), shell=True)
+		call("bowtie2-build {s}/{reference_name} {s}/{reference_name}".format(s=s, reference_name=reference_name), shell=True)
+		call("mkdir {s}/bowtie_reference_files; cd {s}/; for f in *.bt2; do mv $f bowtie_reference_files/$f; done".format(s=s), shell=True)
+		call("bowtie2 -x {s}/bowtie_reference_files/{reference_name} -U {s}/trinity_de_novo_assembly_mapped_reads_only/{fastq1}.nospaces.fastq,{s}/trinity_de_novo_assembly_mapped_reads_only/{fastq2}.nospaces.fastq -S {s}/trinity_de_novo_assembly_mapped_reads_only/{s}.sam --local".format(s=s, fastq1=fastq1, fastq2=fastq2,reference_name=reference_name), shell=True)
+		
+		# put back the spaces so that trinity can use it
+		call("for f in {s}/trinity_de_novo_assembly_mapped_reads_only/*.sam; do sed -i '' $'s/\_1\:N/\ 1\:N/g' $f; done".format(s=s), shell=True)
+		call("for f in {s}/trinity_de_novo_assembly_mapped_reads_only/*.sam; do sed -i '' $'s/\_2\:N/\ 2\:N/g' $f; done".format(s=s), shell=True)
+		
+		# split sam file into forward and reverse and then extract fastqs
+		call("splitsam.sh {s}/trinity_de_novo_assembly_mapped_reads_only/{s}.sam {s}/trinity_de_novo_assembly_mapped_reads_only/{s}.forward.sam {s}/trinity_de_novo_assembly_mapped_reads_only/{s}.reverse.sam {s}/trinity_de_novo_assembly_mapped_reads_only/{s}.unmapped.sam".format(s=s), shell=True)
+		call("reformat.sh in={s}/trinity_de_novo_assembly_mapped_reads_only/{s}.forward.sam out={s}/trinity_de_novo_assembly_mapped_reads_only/{s}.forward.fastq".format(s=s), shell=True)		
+		call("reformat.sh in={s}/trinity_de_novo_assembly_mapped_reads_only/{s}.reverse.sam out={s}/trinity_de_novo_assembly_mapped_reads_only/{s}.reverse.fastq".format(s=s), shell=True)		
+
+		# perform de novo assembly
+		call("/usr/local/bin/trinityrnaseq-Trinity-v2.4.0/Trinity --seqType fq --single {s}/trinity_de_novo_assembly_mapped_reads_only/{s}.forward.fastq,{s}/trinity_de_novo_assembly_mapped_reads_only/{s}.reverse.fastq --max_memory 3G --output {s}/trinity_de_novo_assembly_mapped_reads_only".format(s=s), shell=True)
+
+		# pipe the output to blastn to return the identity of the contigs
+		print "now BLAST searching contigs from Trinity assembly of mapped reads from %s" % s
+		call("/usr/local/bin/ncbi-blast-2.6.0+/bin/blastn -db nt -query {s}/trinity_de_novo_assembly_mapped_reads_only/Trinity.fasta -out {s}/trinity_de_novo_assembly_mapped_reads_only/Trinity_BLAST_result.txt -max_target_seqs 10 -outfmt '7 qseqid sseqid pident length evalue stitle qcovhsp qstart qend sstart send' -remote".format(s=s), shell=True)
+
+
+
+# calculate pi with popoolation
+def pi_analyses():
+	for s in sample_dict:
+		call("mkdir popoolation_analyses".format(), shell=True)
+		call("samtools view -q 0 -bS {s}/{s}.sam | samtools sort {s}/popoolation_analyses/{s}.sorted.bam".format(s=s), shell=True)
+		call("samtools mpileup -d 100000 {s}/popoolation_analyses/{s}.sorted.bam > {s}/popoolation_analyses/{s}.pileup".format(s=s), shell=True)
+		
+		if cfg.calculate_genewise_pi == True:
+			call("perl Variance-at-position.pl --measure pi --pool-size 500 --min-count {min_count} --min-coverage {min_coverage} --max-coverage {max_coverage} --min-qual 0 --gtf --pileup {s}/popoolation_analyses/{s}.pileup --output {s}/popoolation_analyses/{s}.pi.txt".format(s=s, min_coverage=cfg.min_coverage, max_coverage=cfg.max_coverage,min_count=cfg.min_count), shell=True)
+		
+		if cfg.calculate_genewise_piNpiS == True: 
+			call("perl syn-nonsyn/Syn-nonsyn-at-position.pl --measure pi --pool-size 500 --codon-table syn-nonsyn/codon-table.txt --nonsyn-length-table syn-nonsyn/{nonsyn_length_table} --min-count {min_count} --min-coverage {min_coverage} --max-coverage {max_coverage} --min-qual 0 --dissable-corrections --gtf --pilueup {s}/popoolation_analyses/{s}.pileup --output {s}/popoolation_analyses/{s}.syn-nonsyn.txt".format(s=s, min_coverage=cfg.min_coverage, max_coverage=cfg.max_coverage,min_count=cfg.min_count, nonsyn_length_table=cfg.nonsyn_length_table), shell=True)
+		
+		if cfg.calculate_sliding_window_piNpiS == False:
+			call("perl syn-nonsyn/Syn-nonsyn-sliding.pl --measure pi --pool-size 500 --codon-table syn-nonsyn/codon-table.txt --nonsyn-length-table syn-nonsyn/{nonsyn_length_table} --min-count {min_count} --min-coverage {min_coverage} --max-coverage {max_coverage} --min-qual 0 --window-size {pi_window_size} --step-size {pi_step-size} --dissable-corrections --gtf --pileup {s}/popoolation_analyses/{s}.pileup --output {s}/popoolation_analyses/{s}.sliding.txt".format(s=s, min_coverage=cfg.min_coverage, max_coverage=cfg.max_coverage,min_count=cfg.min_count, nonsyn_length_table=cfg.nonsyn_length_table,pi_window_size=cfg.pi_window_size,pi_step_size=cfg.pi_step_size), shell=True)
+
+		if cfg.subsample == True: 
+			call("mkdir {s}/popoolation_analyses/subsampled".format(s=s), shell=True)
+			call("perl basic-pipeline/subsample-pileup.pl --input {s}/popoolation_analyses/{s}.pileup --output {s}/popoolation_analyses/subsampled/{s}.{subsample_level}x.pileup --target-coverage {subsample_level} --max-coverage {max_coverage} --min-qual 0 --method withoutreplace --fastq-type illumina".format(s=s,subsample_level=cfg.subsample_level,min_coverage=cfg.min_coverage,max_coverage=cfg.max_coverage), shell=True)
+			
+			if cfg.calculate_genewise_pi == True:
+				call("perl syn-nonsyn/Syn-nonsyn-at-position.pl --measure pi --pool-size 500 --codon-table syn-nonsyn/codon-table.txt --nonsyn-length-table syn-nonsyn/{nonsyn_length_table} --min-count {min_count} --min-coverage {min_coverage} --max-coverage {max_coverage} --min-qual 0 --dissable-corrections --gtf --pileup {s}/popoolation_analyses/subsampled/{s}.{subsample_level}x.pileup --output {s}/popoolation_analyses/subsampled/{s}.{subsample_level}x.syn-nonsyn.txt".format(s=s, min_coverage=cfg.min_coverage, max_coverage=cfg.max_coverage,min_count=cfg.min_count, nonsyn_length_table=cfg.nonsyn_length_table,subsample_level=cfg.subsample_level), shell=True)
+
+				
+
+# create an output parameter file
+def create_parameter_file():
+	for s in sample_dict:
+		outfile_name = s + ".params.txt"
+		
+		with open(outfile_name, "w") as outfile:
+			outfile.write("")
+		with open(outfile_name, "w") as outfile:
+			outfile.write("TRIMMING: \nMinimum read length after trimming: %s \nTrim window size: %s \nTrim bases to quality threshold of: Q%s \n\n" % (cfg.minlength, cfg.window_size, cfg.trim_qscore))
+			
+			if cfg.use_different_reference_for_each_sample == True:
+				for file in os.listdir(s):
+					if file.endswith(".fasta") or file.endswith(".fa"):
+						reference_name = file
+				outfile.write("REFERENCE SEQUENCE:\n%s\n\n" % file)
+			
+			elif cfg.use_different_reference_for_each_sample == False:
+				outfile.write("REFERENCE SEQUENCE:\n%s\n\n" % cfg.reference_sequence)
+			
+			outfile.write("SNP CALLING AND FILTERING: \nMinimum required coverage: %s\nMinimum quality score for the variant base: %s\nMinimum SNP frequency: %s" % (cfg.min_cov, cfg.snp_qual_threshold, cfg.snp_frequency))
+		outfile.close
+		
+		call("mv {s}.params.txt {s}/{s}.params.txt".format(s=s), shell=True)
+
+			
+				
+
+# RUN THE ANALYSES
+
+combine_fastqfiles()
+
+if cfg.trim == True:
+	trim(sample_list)
+
+if cfg.map == True:
+	map(sample_list)
+
+if cfg.call_snps == True:
+	call_SNPs()
+	
+if cfg.de_novo_assemble_mapped_reads == True:
+	de_novo_assemble_mapped_reads()
+
+create_parameter_file()
+
+if cfg.de_novo_assembly == True:
+	de_novo_assembly()
